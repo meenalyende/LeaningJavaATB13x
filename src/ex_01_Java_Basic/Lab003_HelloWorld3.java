2720f7f86b190684c6e84f400bd4662878964f4f
meenal <meenal.yende@gmail.com>
2025-06-09 22:45:25 +0530

    9th June Classes

---
+++ src/ex_01_Java_Basic/Lab003_HelloWorld3.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab003_HelloWorld3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Meenal and running my second java program");
+    }
+}
+/*Name Meenal
+* Date 9 June 2025*/
---
+++ src/ex_01_Java_Basic/Lab004_HelloWoeld4.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+/*Code Written by Meenal
+* Date 9 June 2025*/
+public class Lab004_HelloWoeld4 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Meenal and running my third java program");
+    }
+}
